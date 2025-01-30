e1c3b4d8a37f5610427be149bddf24a7a3d5300d
thearshadkhan <arshad.cse24@gmail.com>
2025-01-30 23:27:45 +0530

    Get User Route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,6 +5,16 @@ const User = require("../models/User");
 
 const router = express.Router();
 
+//Get user
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find().select("-password");
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
 // Register User
 router.post("/register", async (req, res) => {
     const { name, email, password, role } = req.body;
