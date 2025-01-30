5aecf8d3b6b523f3135ceb5d6a7b3a58a1426ae4
Shivam Goswami <shivamg1008@gmail.com>
2025-01-30 22:28:43 +0530

    Added front-end structure

---
+++ front-end/src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-500">Real Estate App</h1>
+    </div>
+  );
+}
+
+export default App;
