65f5aef37264cec24f092e82dd9a6a295d528226
thearshadkhan <arshad.cse24@gmail.com>
2025-02-05 14:35:46 +0530

    Simple navbar ui updation

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
   return (
 
     <nav className='fixed top-0 w-9/10 bg-red-700 p-4 text-white flex flex-row justify-between ml-15 mt-2 z-10 rounded-full'>
-      <Link to='/'><h1 className="flex text-2xl font-bold px-4 py-2 rounded-full">Brick & Beams<img src={logo} alt="Logo" className="w-7 h-7" />
+      <Link to='/'><h1 className="flex text-2xl font-bold px-4 py-2 rounded-full"><img src={logo} alt="Logo" className="w-7 h-7" />Brick & Beams
       </h1></Link>
 
       {/* Search Bar */}
@@ -25,7 +25,7 @@ const NavBar = () => {
 
       {/* Right Section */}
       <div className="flex gap-4 items-center">
-        <Link to="/properties" className="px-4 py-2  rounded-full cursor-pointer hover:bg-red-900 transition">Add Property →</Link>
+        <Link to="/properties" className="px-4 py-2  rounded-full cursor-pointer hover:bg-red-900 transition">Add Property +</Link>
         <Link to="/Login" className="px-4 py-2  rounded-full cursor-pointer hover:bg-red-900 transition">Login →</Link>
         <Link to="/Register" className="px-4 py-2  rounded-full cursor-pointer hover:bg-red-900 transition">Register →</Link>
       </div>
